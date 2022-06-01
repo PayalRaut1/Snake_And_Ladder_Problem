b0a825cc0d7a43d17442109e30b6f2b88b95b76e
PayaLsRaut <payalraut107@gmail.com>
2022-06-01 07:41:54 +0530

    checked Singal Player Position at 0

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,7 +1,12 @@
 package com.bridgelabz;
 public class SnakeAndLadder
 {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Snake And Ladder Problem");
+    public static final int startPosition = 0;
+    public static final int winningPosition = 100;
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Snake and ladder game");
+        System.out.println("Start position is: "  + startPosition);
+        System.out.println("Winning position is: "  + winningPosition);
     }
 }
