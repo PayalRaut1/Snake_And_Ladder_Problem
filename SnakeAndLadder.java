67f6801777b24c11b08920c841d6c293f0bcd5a1
PayaLsRaut <payalraut107@gmail.com>
2022-06-02 08:17:03 +0530

    Used random method checked for options

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,6 +1,6 @@
 package com.bridgelabz;
 import java.util.Random;
-public class SnakeAndLaddergit
+public class SnakeAndLadder
 {
     public static final int startPosition = 0;
     public static final int winningPosition = 100;
@@ -12,13 +12,35 @@ public class SnakeAndLaddergit
         int value = random.nextInt(6)+1;
         return value;
     }
+    public static int optionForPlay()
+    {
+        Random random=new Random();
+        int value1 = random.nextInt(3);
+        return  value1 ;
+    }
 
     public static void main(String[] args)
     {
         System.out.println("Welcome to Snake and ladder game");
         System.out.println("Start position is: "  + startPosition);
         System.out.println("Winning position is: "  + winningPosition);
-        position = position + throwDie();
+
+        int playValue=optionForPlay();
+        if(playValue==0)
+        {
+            System.out.println("No play");
+            position = position + throwDie();
+        }
+        else if(playValue==1)
+        {
+            System.out.println("Got Ladder");
+            position = position + throwDie();
+        }
+        else if(playValue==2)
+        {
+            System.out.println("Snake Attack");
+            position = position + throwDie();
+        }
         System.out.println("new position is : " +position);
 
     }
